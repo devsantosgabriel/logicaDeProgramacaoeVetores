61cfe6eecd48c086233a140e96e7b4d19c8862fa
devsantosgabriel <euinstrutorgabriel@gmail.com>
2023-10-03 11:18:12 -0300

    feat: criando objeto usando classe

---
+++ objeto/objeto-class.js
@@ -0,0 +1,10 @@
+class Objeto_Class{
+    constructor(nome, idade){
+        this.nome = nome,
+        this.idade = idade
+    }
+};
+
+let instancia_objeto = new Objeto_Class('Gabriel', 29);
+
+console.log(instancia_objeto.nome);
